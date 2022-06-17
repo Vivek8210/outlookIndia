96f11499ec2ed8be13cf2c8c94bf925dfad75353
Vivek8210 <99641318+Vivek8210@users.noreply.github.com>
2022-06-17 11:43:43 +0530

    signIn signup done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,7 +4,8 @@ import Footer from './footer/Footer';
 import Navbar from './components/Navbar'
 import AllRoutes from './components/AllRoutes';
 import OutlookLogo from './components/OutlookLogo';
-
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
   return (
     <div >
--- my-app/src/redux/store.js
+++ my-app/src/redux/store.js
@@ -1,9 +1,11 @@
 
 import {legacy_createStore,combineReducers,applyMiddleware,compose} from "redux"
 import thunk from "redux-thunk"
-import newsReducer from "./reducer"
+import newsReducer from "./reducer";
+import authReducer from "./reducer"
 
-const rootReducer=combineReducers({magzineData:newsReducer})
+const rootReducer=combineReducers({magzineData:newsReducer,
+     authReducer})
 
  const composeEnhancers=window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose
 
